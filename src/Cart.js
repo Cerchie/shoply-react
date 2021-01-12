ecc5e996fca9fc9351a2aaa468cdc514f838af7e
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-12 12:41:22 -0700

    added ability to send str to cart from CART

--- src/Cart.js
+++ src/Cart.js
@@ -2,7 +2,20 @@ import React from 'react';
 import { useSelector, useDispatch } from "react-redux";
 
 function Cart(){
+    const dispatch = useDispatch();
     const cart_items = useSelector(st => st.cart_items)
+    function addItem() {
+        dispatch({ type: "ADD_TO_CART", cart_item:{ "name" : "another"}});
+      
+      }
+      function deleteItem() {
+        dispatch({ type: 'REMOVE_FROM_CART' });
+      }
+      function handleAdd(e){
+          e.preventDefault();
+          addItem()
+      }
+ 
     console.log(cart_items)
     return (
         <div>
@@ -12,6 +25,8 @@ function Cart(){
                 {i.price} 
                 <img width="100" src={i.image_url}/>
                 {i.description}</div>)}
+                <button onClick={addItem}>Add another</button>
+                <button onClick={deleteItem}>remove all from cart</button>
         </div>
     )
 }
