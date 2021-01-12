ee07b8782e5b7b570f819374b2732ef6a15583b5
Lucia Cerchie <luciacerchie@gmail.com>
2021-01-12 11:21:11 -0700

    creates product detail and cart comps to work on

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 function ProductDetail(){
+    const total_products = useSelector(st => st.total_products)
+    let arrays_to_map = Object.entries(total_products.products);
+    const { id } = useParams();
     return (
         <div>
-            details here
+             {arrays_to_map[0].map(p => (
+    <div
+      key={p.id}
+      name={p.name}
+      price={p.price}
+      description={p.description}
+      image_url={p.image_url}
+    />  
+        ))}
         </div>
     )
 }
